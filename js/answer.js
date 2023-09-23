c1555a6db18c148344fa3ed17be1d3b503c65b47
barin2010 <uyrirudenko@gmail.com>
2023-09-24 00:19:16 +0300

    add

--- js/answer.js
+++ js/answer.js
@@ -1,43 +1,44 @@
-//  const answersToggle = document.querySelector('.answers-toggle');
-//   const answersSvg = document.querySelector('.answers-svg');
-//   const answersText = document.querySelector('.answers-text');
+// Отримуємо список усіх SVG-елементів
+const svgs = document.querySelectorAll(".answers-svg");
 
-//   answersToggle.addEventListener('click', function() {
-//     const isHidden = answersText.style.display === 'none';
+// Додаємо обробник події для кожного SVG
+svgs.forEach((svg) => {
+  svg.addEventListener("click", () => {
+    // Знаходимо батьківський елемент li, який містить SVG
+    const listItem = svg.closest(".answers-item");
 
-//     answersText.style.display = isHidden ? 'block' : 'none';
-//     answersSvg.innerHTML = isHidden ?
-//       <svg xmlns="http://www.w3.org/2000/svg" width="18" height="17" viewBox="0 0 18 17" fill="none">
-//         <path d="M9 0.5V16.5" stroke="#E1016E" stroke-linecap="round"/>
-//         <path d="M17 8.5L0.999999 8.5" stroke="#E1016E" stroke-linecap="round"/>
-//       </svg>
-//      :
-//       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
-//         <path d="M24 16L8 16" stroke="#E1016E" stroke-linecap="round"/>
-//       </svg>
-//     ;
-//   });
+    // Знаходимо answers-content для цього елемента li
+    const content = listItem.querySelector(".answers-content");
 
-// Найти все элементы аккордеона
-const accordionItems = document.querySelectorAll('.accordion-item');
-
-// Прикрепить обработчик клика к каждому элементу аккордеона
-accordionItems.forEach((item) => {
-  const collapse = item.querySelector('.accordeon-collapse');
-  const accordionText = item.querySelector('.accordion-text');
-
-  // При клике на "Подробнее"
-  collapse.addEventListener('click', () => {
-    if (accordionText.style.display === 'block') {
-      // Если аккордеон уже открыт, закрыть его
-      accordionText.style.display = 'none';
-      collapse.querySelector('.accordeon-collapse-text').textContent = 'Подробнее';
-      collapse.classList.remove('open');
+    // Змінюємо стиль answers-content на "display: block" або "display: none"
+    if (content.style.display === "none" || content.style.display === "") {
+      content.style.display = "block";
+      svg.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+          <path d="M24 16L8 16" stroke="#E1016E" stroke-linecap="round"/>
+        </svg>`;
     } else {
-      // Если аккордеон закрыт, открыть его
-      accordionText.style.display = 'block';
-      collapse.querySelector('.accordeon-collapse-text').textContent = 'Закрыть';
-      collapse.classList.add('open');
+      content.style.display = "none";
+      svg.innerHTML = `
+        <svg
+          class="answers-svg"
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="17"
+          viewBox="0 0 18 17"
+          fill="none"
+        >
+          <path
+            d="M9 0.5V16.5"
+            stroke="#E1016E"
+            stroke-linecap="round"
+          />
+          <path
+            d="M17 8.5L0.999999 8.5"
+            stroke="#E1016E"
+            stroke-linecap="round"
+          />
+        </svg>`;
     }
   });
 });
